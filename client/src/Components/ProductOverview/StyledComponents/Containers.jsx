26fff23823d13650fa8edaad266e20551f69df9d
Panlord <adpan.dev@gmail.com>
2022-07-09 16:22:48 -0700

    add comments to styled components file

--- client/src/Components/ProductOverview/StyledComponents/Containers.jsx
+++ client/src/Components/ProductOverview/StyledComponents/Containers.jsx
@@ -1,13 +1,19 @@
+// All of the styled components here are flexbox containers to organize their respective contents in certain ways
+
+// Import any relevant methods from the styled-components library
 import styled from 'styled-components';
 
+// Container to hold the Image Gallery and Product Information (except description) components
 const ProductOverviewContainer = styled.section`
   display: flex;
   background-color: lightblue;
 `;
 
+// Container to hold the Product Information description component
 const ProductInformationDescription = styled.section`
   display: flex;
   background-color: cyan;
 `;
 
+// Export the styled components
 export { ProductOverviewContainer, ProductInformationDescription };
