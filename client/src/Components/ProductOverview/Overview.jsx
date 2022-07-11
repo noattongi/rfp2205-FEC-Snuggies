e7cd66018ef33ed2981383a9fb74c858b1e555e5
Panlord <adpan.dev@gmail.com>
2022-07-11 02:43:47 -0700

    incorporate data retrieved from server to Information component

--- client/src/Components/ProductOverview/Overview.jsx
+++ client/src/Components/ProductOverview/Overview.jsx
@@ -13,13 +13,26 @@ var Overview = (props) => {
   // Upon component mounting, send a GET request and get all the relevant data (this may need to be refactored if the team decides to house all client-side request handling in the main App file)
   useEffect(() => {
     console.log('HI!');
-    // Send axios request to get all products? or maybe just the specific one
-    // Then set the products state
-    // Send axios request to get all styles
-    // Then set the styles state
-    // Send axios request to get all the review data that is relevant for the specific product
-    // Then set the reviews state
-  });
+    // Send axios request to the specific product
+    axios.get('/snuggie/products', { params: { product_id: 40344 } }) // Right now, we have a placeholder for the specific product
+      // Then set the products state
+      .then((results) => {
+        setProduct(results.data);
+      })
+      // Send axios request to get all styles
+      .then(() => {
+        return axios.get('/snuggie/styles', { params: { product_id: 40344 } });
+      })
+      // Then set the styles state
+      .then((results) => {
+        setStyles(results.data.results);
+      })
+      // Send axios request to get all the review data that is relevant for the specific product
+      // Then set the reviews state
+      .catch((error) => {
+        console.log('An error occurred when initializing data received from server:', error);
+      });
+  }, []); // The second argument is an empty array, which makes it so that we only run the function once
 
   return (
     <div>
