2ac85e340c1d79b1ab9db93a671ab4e20004b44b
David Arredondo <dcarredondo90@gmail.com>
2022-07-11 13:45:43 -0700

    hardcoded Product ID

--- client/src/Components/RatingsAndReviews/RatingsAndReviewsIndex.jsx
+++ client/src/Components/RatingsAndReviews/RatingsAndReviewsIndex.jsx
@@ -4,6 +4,18 @@ import ReactDOM from 'react-dom';
 import ReviewList from '../RatingsAndReviews/ReviewList/ReviewList.jsx'
 
 var RatingsAndReviewsIndex = (props) => {
+
+  const getProductReviews = (productId) => {
+    return axios.get('/snuggie/reviews/', {params: {product_id: '?product_id=' + productId}})
+    .then((response) => {
+      setProduct(response.data);
+    })
+    .catch((error) => {
+      console.log('Error in getProduct', error)
+    })
+  }
+
+
   return (
     <ReviewList/>
   )
