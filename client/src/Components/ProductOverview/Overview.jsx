f0e940000c6c9faf7808c524841cea9dd95a7e97
Panlord <adpan.dev@gmail.com>
2022-07-11 02:13:29 -0700

    import axios and pseudocode useEffect method

--- client/src/Components/ProductOverview/Overview.jsx
+++ client/src/Components/ProductOverview/Overview.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { ProductOverviewContainer, ProductInformationDescription } from './StyledComponents/Containers.jsx';
 import Gallery from './ImageGallery/Gallery.jsx';
 import Information from './ProductInformation/Information.jsx';
@@ -8,6 +9,17 @@ var Overview = (props) => {
   const [product, setProduct] = useState({});
   const [styles, setStyles] = useState([]);
   const [reviews, setReviews] = useState({});
+
+  // Upon component mounting, send a GET request and get all the relevant data (this may need to be refactored if the team decides to house all client-side request handling in the main App file)
+  useEffect(() => {
+    console.log('HI!');
+    // Send axios request to get all products? or maybe just the specific one
+    // Then set the products state
+    // Send axios request to get all styles
+    // Then set the styles state
+    // Send axios request to get all the review data that is relevant for the specific product
+    // Then set the reviews state
+  });
 
   return (
     <div>
