9c756d38eb69eb74302885f72281f27b3e2a9fdd
David Arredondo <dcarredondo90@gmail.com>
2022-07-11 19:34:45 -0700

    feat: Add funtionality to More Reviews Button
           Add funtionality to More Reviews Button
    
                type:feat

--- client/src/Components/RatingsAndReviews/ReviewList/ReviewList.jsx
+++ client/src/Components/RatingsAndReviews/ReviewList/ReviewList.jsx
@@ -4,16 +4,34 @@ import ReactDOM from 'react-dom';
 import ReviewTile from './ReviewTiles.jsx'
 
 var ReviewList = (props) => {
-  var limitReviews = props.productReviews.results?.slice(0,2)
+  const [reviewCount, setReviewCount] = useState(2);
+
+
+  var moreReviewClick = (event) => {
+    setReviewCount(reviewCount + 2)
+  }
+  var limitReviews = props.productReviews.results?.slice(0,reviewCount)
+  // console.log(props.productReviews.results)
+  var moreReviewToggle = (reviewLength) => {
+    var moreReviewbutton;
+    console.log(reviewCount, 'lesss athan ',reviewLength, 'hellooo')
+    if(reviewLength > 2 && reviewCount < reviewLength) {
+      moreReviewbutton = <button onClick={moreReviewClick} >More Reviews</button>
+      return moreReviewbutton
+    } else {
+      return moreReviewbutton;
+    }
+  }
+
   return (
     <div>
     <ul>
       {limitReviews?.map((review, index) =>
         <ReviewTile key={index}
-                  reviews={review} />
+                  reviews={review}/>
       )}
     </ul>
-    <button>More Reviews</button>
+    {moreReviewToggle(props.productReviews.results?.length)}
     <button>Add A Review</button>
     </div>
   )
