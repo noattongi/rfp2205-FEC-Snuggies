991a0b649ddde941ba50af5078a4c34f1f435ea1
JASPER <jasjasper101@gmail.com>
2022-07-13 15:50:52 -0700

    feat: Created a text area, user input, email input, upload button, and submit button for the answer modal

--- client/src/Components/QuestionsAndAnswers/QuestionWithAnswers/AddAnswer.jsx
+++ client/src/Components/QuestionsAndAnswers/QuestionWithAnswers/AddAnswer.jsx
@@ -2,8 +2,13 @@ import React, {useState} from 'react'
 import styled from 'styled-components'
 // modal
 
+
+
 // subtitle: needs product name and question body
 var AddAnswer = ({q, toggleModal}) => {
+  var [answerEntry, setAnswerEntry] = useState('');
+  var [username, setUsername] = useState('');
+  var [email, setEmail] = useState('');
   return (
     <div>
       <StyleBackground>
@@ -15,6 +20,26 @@ var AddAnswer = ({q, toggleModal}) => {
                 <ProductName> Yeezys:  </ProductName>
                 <QuestionBody> {q} </QuestionBody>
             </ModalSubtitleContainer>
+            <ModalBody>
+            <UserInfoContainer>
+              <UserNameContainer>
+              <UserNameLabel> User Nickname</UserNameLabel>
+                <UserNameInput value={username} onChange={e => setUsername(e.target.value)} maxlength='60' placeholder='Example: jack543!' />
+                <NameWarningSpan>For privacy reasons, do not use <br/> your full name </NameWarningSpan>
+              </UserNameContainer>
+              <EmailContainer>
+                <EmailLabel> Email Address </EmailLabel>
+                <EmailInput value={email} onChange={e => setEmail(e.target.value)} maxlength='60' placeholder='Example: jack@email.com'/>
+                <EmailWarningSpan> For authentication reasons, you will not be emailed</EmailWarningSpan>
+              </EmailContainer>
+
+            </UserInfoContainer>
+            <AnswerBody required='' maxlength= '1000' onChange={e => setAnswerEntry(e.target.value)} value={answerEntry} placeholder='Add your answer here...'> </AnswerBody>
+            <BottomButtonContainers>
+                <UploadButton> Upload Images </UploadButton>
+                <SubmitButton> Submit! </SubmitButton>
+            </BottomButtonContainers>
+            </ModalBody>
          </ModalHeader>
 
          </ModalContent>
@@ -31,7 +56,6 @@ var StyleBackground = styled.div`
   z-index: 10;
   left: 0;
   top: 0;
-  padding-top: 100px;
   width: 100%;
   height: 100%;
   overflow: auto;
@@ -40,11 +64,70 @@ var StyleBackground = styled.div`
   background-color: rgba(0,0,0,0.4);
 `;
 
+var UserNameContainer = styled.div`
+  display: flex;
+  flex-direction: column
+`;
+
+
+var EmailContainer = styled.div`
+  display: flex;
+  flex-direction: column
+`;
+
+var UploadButton = styled.button`
+
+`;
+
+var NameWarningSpan = styled.span`
+  font-size 12px;
+`;
+
+var EmailWarningSpan = styled.span`
+  font-size 12px;
+`;
+var UserInfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 10px;
+`;
+
+var UserNameLabel = styled.label`
+  font-weight: bold;
+`;
+
+var EmailLabel = styled.label`
+  font-weight: bold;
+`;
+
+var UserNameInput = styled.input`
+  width: 175px;
+`;
+
+var SubmitButton = styled.button`
+
+`;
+
+var EmailInput = styled.input`
+width: 280px;
+`;
+var AnswerBody = styled.textarea`
+  width: 700px;
+  height: 300px;
+`;
+
+var BottomButtonContainers = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 10px;
+`;
 var CloseButton = styled.button`
   color: #aaa;
   font-size: 28px;
   font-weight: bold;
-  height: 43%;
+  height: 38px;
+  position: relative;
+  right: 200px;
   :hover {
     color: black;
     text-decoration: none;
@@ -56,7 +139,10 @@ var CloseButton = styled.button`
     cursor: pointer;
   }
 `;
-
+var ModalBody = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 var ModalHeader = styled.div`
   display: flex;
   flex-direction: column;
@@ -69,22 +155,25 @@ var ModalH2 = styled.h2`
 var ModalSubtitleContainer = styled.div`
   display: flex;
   flex-direction: row;
+  width: 500px;
+  justify-content: space-around;
 `;
 
-var ProductName = styled.span`
+var ProductName = styled.h3`
 
 `;
 
-var QuestionBody = styled.span`
+var QuestionBody = styled.h3`
 
 `;
 
 var ModalContent = styled.div`
   display: flex;
   background-color: #fefefe;
-  margin: 15% auto;
+  margin: 2% auto;
   padding: 20px;
   border: 1px solid #888;
   width: 80%;
+  justify-content: center;
 `;
 export default AddAnswer
