65a80a443384aa3fcbefd1fba9d4e17b841420d3
Clayton Chin <chinclayton98@gmail.com>
2022-07-14 12:39:58 -0700

    feat: added testing files

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Overview from './Components/ProductOverview/Overview.jsx';
+import QnaIndex from './Components/QuestionsAndAnswers/QnaIndex.jsx';
+import RatingsAndReviewsIndex from './Components/RatingsAndReviews/RatingsAndReviewsIndex.jsx'
+import RInC from './Components/RelatedItemsAndComparison/RInCIndex.jsx';
+
+var App = () => {
+  return(
+    <div>
+      <Overview />
+      <br/>
+      <RInC/>
+      <br/>
+      <QnaIndex/>
+      <br/>
+      <RatingsAndReviewsIndex/>
+    </div>
+  )
+}
+
+export default App;
