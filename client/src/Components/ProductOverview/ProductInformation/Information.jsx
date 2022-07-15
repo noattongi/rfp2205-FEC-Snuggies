a4545a54cb1b1c087650612d29afcbc5b5b5cde5
Panlord <adpan.dev@gmail.com>
2022-07-15 15:49:25 -0700

    refactor: default selected style is the first one in the list

--- client/src/Components/ProductOverview/ProductInformation/Information.jsx
+++ client/src/Components/ProductOverview/ProductInformation/Information.jsx
@@ -14,15 +14,19 @@ var Information = (props) => {
   var salePrice;
   // Iterate and choose the default style whenever the style list changes (e.g. choose new product)
   useEffect(() => {
-    // For each of the styles in the passed in styles prop, iterate until we find the default one and set that as the displayed style
-    for (let i = 0; i < props.styles.length; i++) {
-      if (props.styles[i]["default?"]) {
-        props.setChosenStyle(props.styles[i]);
-        break;
-      }
+    // // For each of the styles in the passed in styles prop, iterate until we find the default one and set that as the displayed style
+    // for (let i = 0; i < props.styles.length; i++) {
+    //   if (props.styles[i]["default?"]) {
+    //     props.setChosenStyle(props.styles[i]);
+    //     break;
+    //   }
+    // }
+    if (props.styles.length > 0) {
+      // Per business requirements document, the default style selected is the first one in the list
+      props.setChosenStyle(props.styles[0]);
+      // Then get the sale price
+      salePrice = props.chosenStyle.sale_price;
     }
-    // Then get the sale price
-    salePrice = props.chosenStyle.sale_price;
   }, [props.styles]);
 
   // Generate the price component
