ce6bf467715967f88e18000d11b28a0047a4f8c4
Panlord <adpan.dev@gmail.com>
2022-07-14 21:23:43 -0700

    refactor Product Overview widget to use the productId hook prop passed down from the App

--- client/src/Components/ProductOverview/Overview.jsx
+++ client/src/Components/ProductOverview/Overview.jsx
@@ -14,26 +14,28 @@ var Overview = (props) => {
   // Upon component mounting, send a GET request and get all the relevant data (this may need to be refactored if the team decides to house all client-side request handling in the main App file)
   useEffect(() => {
     console.log('HI!');
-    // Send axios request to the specific product
-    axios.get('/snuggie/products', { params: { product_id: 40344 } }) // Right now, we have a placeholder for the specific product
-      // Then set the products state
-      .then((results) => {
-        return setProduct(results.data);
-      })
-      // Send axios request to get all styles
-      .then(() => {
-        return axios.get('/snuggie/styles', { params: { product_id: 40344 } });
-      })
-      // Then set the styles state
-      .then((results) => {
-        return setStyles(results.data.results);
-      })
-      // Send axios request to get all the review data that is relevant for the specific product
-      // Then set the reviews state
-      .catch((error) => {
-        console.log('An error occurred when initializing data received from server:', error);
-      });
-  }, []); // The second argument is an empty array, which makes it so that we only run the function once
+    if (props.productId) {
+      // Send axios request to the specific product
+      axios.get('/snuggie/products', { params: { product_id: props.productId } }) // Right now, we have a placeholder for the specific product
+        // Then set the products state
+        .then((results) => {
+          return setProduct(results.data);
+        })
+        // Send axios request to get all styles
+        .then(() => {
+          return axios.get('/snuggie/styles', { params: { product_id: props.productId } });
+        })
+        // Then set the styles state
+        .then((results) => {
+          return setStyles(results.data.results);
+        })
+        // Send axios request to get all the review data that is relevant for the specific product
+        // Then set the reviews state
+        .catch((error) => {
+          console.log('An error occurred when initializing data received from server:', error);
+        });
+    }
+  }, [props.productId]); // The second argument is an empty array, which makes it so that we only run the function once
 
   return (
     <div>
