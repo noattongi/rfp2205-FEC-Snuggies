ac56cc1e0d0159326ba6b6cc7a232782a605e287
Panlord <adpan.dev@gmail.com>
2022-07-15 19:57:26 -0700

    add server route for adding product to cart

--- server/routes.js
+++ server/routes.js
@@ -196,5 +196,20 @@ router.put('/report', (request, response) => {
   })
 });
 
+// Adds item to cart
+router.post('/cart', (request, response) => {
+  if (!request.body.sku_id) {
+    response.sendStatus(500);
+  }
+  API.addToCart(request.body.sku_id)
+    .then((results) => {
+      response.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log('Error occurred when attempting to add item to cart', error)
+      response.sendStatus(500)
+    });
+});
+
 
 module.exports = router;
