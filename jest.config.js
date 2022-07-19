e61c1042198d02911a70e8ce12e1800a6561af8c
Panlord <adpan.dev@gmail.com>
2022-07-19 16:39:54 -0700

    build: configure package.json script and jest config to show coverage for relevant files after testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  collectCoverageFrom: ['./client/src/**/*.js',
+                        './client/src/**/*.jsx',
+                        "**/*.{js,jsx}",
+                        "!**/node_modules/**",
+                        "!**/*.config.js",
+                        "!**/bundle.js",
+                        "!**/coverage/**"],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
