c6ad25add11eba0364d8c5fba210b4f5360fe357
Panlord <adpan.dev@gmail.com>
2022-07-19 02:30:26 -0700

    feat: implement add to cart button functionality

--- client/src/Components/ProductOverview/ProductInformation/AddToCart.jsx
+++ client/src/Components/ProductOverview/ProductInformation/AddToCart.jsx
@@ -2,6 +2,7 @@
 
 // Import stuff
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { SizeAndCountContainer, AddToCartContainer } from '../StyledComponents/Containers.jsx';
 import SizeDropdown from '../StyledComponents/ProductInformation/AddToCart/SizeDropdown.jsx';
 import QuantityDropdown from '../StyledComponents/ProductInformation/AddToCart/QuantityDropdown.jsx';
@@ -11,11 +12,10 @@ import FavoriteButton from '../StyledComponents/ProductInformation/AddToCart/Fav
 // The component
 var AddToCart = (props) => {
 
-  // Try deconstructing props & initializing to empty obj
-
   const [skus, setSkus] = useState([]);
   const [chosenSize, setChosenSize] = useState();
   const [chosenQuantity, setChosenQuantity] = useState();
+  const [clickedWithoutSize, setClicked] = useState(false);
 
   var style = props.chosenStyle;
 
@@ -43,11 +43,29 @@ var AddToCart = (props) => {
     }
   }
 
+  // Function to handle clicking AddToCart button
+  var handleAddToCart = () => {
+    // If no size is selected, open the dropdown and display a message
+    if (!chosenSize) {
+      setClicked(true);
+    } else {
+    // If a valid size and quantity are selected, send a POST request to the API (adding item(s) to cart)
+      axios.post('/snuggie/cart', { "sku_id": chosenSize, "count": chosenQuantity })
+        .then(() => {
+          console.log(`Added ${chosenQuantity} items of size ${style.skus[chosenSize].size} to cart.`);
+        })
+        .catch((error) => {
+          console.log(`Error in adding  ${chosenQuantity} items of size ${style.skus[chosenSize].size} to cart.`);
+        });
+    }
+  }
+
   return (
     <div>
       <SizeAndCountContainer>
-        <SizeDropdown name="size" id="size-select" onChange={(event) => {setChosenSize(event.target.value)}}>
-          <option value="">--Please select a size--</option>
+        {clickedWithoutSize && <span>Please select size</span>}
+        <SizeDropdown name="size" id="size-select" onChange={(event) => {setChosenSize(event.target.value); setClicked(false);}}>
+          <option value="">Select Size</option>
           {/* For each sku_id, add a size */}
           {skus.map((sku_id, index) => {
             if (style.skus[sku_id.toString()]) {
@@ -56,15 +74,15 @@ var AddToCart = (props) => {
           })}
         </SizeDropdown>
         <QuantityDropdown name="quantity" id="quantity-select" disabled={!chosenSize} onChange={(event) => {setChosenQuantity(event.target.value)}}>
-          <option value="">-</option>
+          <option value="0">-</option>
           {/* Add options for the quantity based on how many are available for the selected size */}
           {quantityOptions.map((quantity) => {
             return quantity;
           })}
         </QuantityDropdown>
       </SizeAndCountContainer>
-      {chosenSize && chosenQuantity && <AddToCartContainer>
-        <AddToCartButton>Add to Cart</AddToCartButton>
+      {(chosenQuantity !== 0) && <AddToCartContainer>
+        <AddToCartButton onClick={() => {handleAddToCart()}}>Add to Cart</AddToCartButton>
       </AddToCartContainer>}
     </div>
   );
