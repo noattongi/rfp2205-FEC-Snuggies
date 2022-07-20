93a493883365f5d13011e744bfec7541ef428d0b
Panlord <adpan.dev@gmail.com>
2022-07-20 10:08:01 -0700

    improve accuracy of horizontal panning during zoom

--- client/src/Components/ProductOverview/StyledComponents/ImageGallery/ImageZoom.jsx
+++ client/src/Components/ProductOverview/StyledComponents/ImageGallery/ImageZoom.jsx
@@ -34,16 +34,28 @@ const Magnifier = styled.div`
   background-size: ${(props) => {return (props.imageWidth * props.magnifier)}}px ${(props) => {return (props.imageHeight * props.magnifier)}}px;
   background-position-x: ${(props) => {
                             var zoomPosition = props.x * (-1) * props.magnifier + (100/document.documentElement.clientWidth)/2;
-
-                            if (zoomPosition >= (-1 * props.imageWidth)) {
+                            console.log(zoomPosition, props.imageWidth);
+                            // If the width of the image is smaller than the size of the viewport, just render the whole width
+                            if ((props.imageWidth * props.magnifier) <  (100/document.documentElement.clientWidth)) {
+                              return 0;
+                            }
+                            if (zoomPosition - props.imageWidth >= 0) {
+                              console.log('in up');
                               return zoomPosition;
                             } else {
-                              return (-1 * props.imageWidth);
+                              console.log('in down');
+                              return props.imageWidth * (props.magnifier - 1) + (100/document.documentElement.clientWidth) + zoomPosition - props.imageWidth*.75;
                             }
+                            // if (zoomPosition > props.imageWidth * -1 * (props.magnifier - 1)) {
+                            //   return zoomPosition;
+                            // } else {
+                            //   console.log('RAH!', props.imageWidth * -1 * (props.magnifier) + (100/document.documentElement.clientWidth));
+                            //   return props.imageWidth * -1 * (props.magnifier) + (100/document.documentElement.clientWidth) - zoomPosition;
+                            // }
+                            // return props.imageWidth * (props.magnifier - 1) + (100/document.documentElement.clientWidth) + zoomPosition;
                           }}px;
   background-position-y: ${(props) => {
                             var zoomPosition = props.y * (-1) * props.magnifier + 740/2;
-                            console.log(zoomPosition, (props.magnifier - 1) * -1 * props.imageHeight);
                             if (zoomPosition > 0) {
                               return 0;
                             } else if (zoomPosition >= ((props.magnifier - 1) * -1 * props.imageHeight)) {
