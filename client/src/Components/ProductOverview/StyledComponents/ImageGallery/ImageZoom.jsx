43261a32c642a81bdbc615919c0068d3e162ac98
Panlord <adpan.dev@gmail.com>
2022-07-20 05:40:13 -0700

    zoom functionality WIP

--- client/src/Components/ProductOverview/StyledComponents/ImageGallery/ImageZoom.jsx
+++ client/src/Components/ProductOverview/StyledComponents/ImageGallery/ImageZoom.jsx
@@ -27,11 +27,27 @@ const Magnifier = styled.div`
   pointer-events: none;
   height: 740px;
   width: 100vw;
+  top: 0;
+  left: 0;
   background-image: url(${(props) => props.imageUrl});
   background-repeat: no-repeat;
   background-size: 100vw 1850px;
-  background-position-x: ${(props) => {return (props.x * (-1) * props.magnifier + (100/document.documentElement.clientWidth)/2)}}px;
-  background-position-y: ${(props) => {return (props.y * (-1) * props.magnifier + 740/2)}}px;
+  background-position-x: ${(props) => {
+                            var zoomPosition = props.x * (-1) * props.magnifier + (100/document.documentElement.clientWidth)/2;
+                            if (zoomPosition >= (100/document.documentElement.clientWidth)) {
+                              return zoomPosition;
+                            } else {
+                              return 0;
+                            }
+                          }}px;
+  background-position-y: ${(props) => {
+                            var zoomPosition = props.y * (-1) * props.magnifier + (100/document.documentElement.clientWidth)/2;
+                            if (zoomPosition <= 0) {
+                              return zoomPosition;
+                            } else {
+                              return 0;
+                            }
+                          }}px;
   border: solid 3px black;
   z-index: 25;
 `;
